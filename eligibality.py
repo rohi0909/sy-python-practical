142438a618f49e97f2b446303d6d8cd6c994879c
rohi0909 <rohininikam1521@gmail.com>
2026-07-29 13:03:22 +0530

    Add files via upload

---
+++ eligibality.py
@@ -0,0 +1,19 @@
+print("----------student details-------")
+name=(input("Enter your name:"))
+age=0
+age=(input("Enter your age:"))
+if age<=17 and age>=25:
+    print("it is elgibal")
+else:
+    print("it is not elgibal")  
+    
+mark=input("enter your mark")    
+mark=0
+if mark>=90:
+     print("allow seat in aiml",mark) 
+elif mark>=80:
+    print("allow seat in cse",mark)
+else :mark>=60  
+print("allow seat in genral",mark)    
+    
+       
