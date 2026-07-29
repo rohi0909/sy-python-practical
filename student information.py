7c80cddf2d15630b654e07780f29a58f1406722b
rohi0909 <rohininikam1521@gmail.com>
2026-07-29 13:00:19 +0530

    Add files via upload

---
+++ student information.py
@@ -0,0 +1,17 @@
+print("--------student Details-------")
+
+Name=input ("Enter your name:")
+
+Age=input("Enter your Age:")
+
+City=input("Enter your City:")
+
+PRN=input("Enter your PRN:")
+
+
+
+print("-------Show student Details-------")
+print(f"Name:{Name}")
+print(f"Age:{Age}")
+print(f"City:{City}")
+print(f"PRN:{PRN}")
